3e24016021d09f1080d7f2f917cf61581c02f1aa
iLofiz <iloaf9820@gmail.com>
2025-11-14 19:23:59 +0000

    Files

---
+++ src/main/java/dev/lofiz/pride/Pride.java
@@ -0,0 +1,17 @@
+package dev.lofiz.pride;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Pride implements ModInitializer {
+
+	public static final String MOD_ID = "pride";
+	public static final Logger logger = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/dev/lofiz/pride/data/PrideDataGenerator.java
@@ -0,0 +1,12 @@
+package dev.lofiz.pride.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PrideDataGenerator implements DataGeneratorEntrypoint {
+
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
